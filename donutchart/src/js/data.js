5287acb8ac92002b718a82da5358986dcaf4848a
Guillaume Guarin <guillaume.guarin@webdrone.fr>
2022-09-06 16:49:20 +0200

    Element lit et donut chartjs

---
+++ donutchart/src/js/data.js
@@ -0,0 +1,74 @@
+export const DOUGHNUT_CONFIG = {
+  type: "doughnut",
+  result: Math.floor(Math.random() * 100),
+  data: {
+
+    names: [
+      'Apple',
+      'Windows',
+      'Nokia',
+      'Huawai',
+      'Android',
+      'linux'
+    ],
+    datasets: [{
+      label: 'Donut',
+      data: [Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100),
+      Math.floor(Math.random() * 100)],
+      backgroundColor: [
+
+        '#0D94AD',
+        '#7b4c9f',
+        '#cc18e0',
+        '#F14665',
+        '#F1960C',
+        '#4c9f62'
+
+      ],
+      links: [
+        '',
+        'http://microsoft.com',
+        'http://nokia.com',
+        'http://huawai.com',
+        'http://google.com',
+        'http://linux.org'
+
+      ],
+      hoverOffset: 4,
+      doughnut: {
+        borderWidth: 100
+      }
+    }]
+  },
+  options: {
+
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      datalabels: {
+        color: "#ffffff",
+        font: {
+          size: 18,
+          weight: "bold"
+        }
+      }
+    },
+    legend: {
+      display: false
+    },
+    title: {
+      display: false
+    },
+    animation: {
+      animateScale: true,
+      animateRotate: true
+    },
+    tooltips: {
+      enabled: false
+    }
+  }
+};
---
+++ donutchart/src/js/mv-donut-chart.js
@@ -0,0 +1,492 @@
+import {LitElement, html, css} from "https://cdn.jsdelivr.net/gh/meveo-org/mv-dependencies@master/lit-element.js";
+import "../lib/chart.min.js";
+import "../lib/chartjs-plugin-datalabels.min.js";
+import {DOUGHNUT_CONFIG} from "./data.js";
+
+export class MvChart extends LitElement {
+    static get properties() {
+        return {
+            type: {
+                type: String,
+                attribute: true
+            },
+            data: {
+                type: Object,
+                attribute: false,
+                reflect: true
+            },
+            options: {
+                type: Object,
+                attribute: false,
+                reflect: true
+            },
+            plugins: {
+                type: Object,
+                attribute: false,
+                reflect: true
+            },
+
+            //  valid theme values are: "light", "dark"    default: "light"
+            theme: {
+                type: String,
+                attribute: true
+            }
+        };
+    }
+
+    static get styles() {
+        return css `
+      :host {
+        font-family: var(--font-family, Arial);
+        font-size: var(--font-size-m, 10pt);
+        --chart-margin: var(--mv-chart-margin, auto);
+        --chart-height: var(--mv-chart-height, 300px);
+        --chart-width: var(--mv-chart-width, 400px);
+        --light-background: var(--mv-chart-background, #ffffff);
+        --dark-background: var(--mv-chart-dark-background, #373e48);
+        --light-color: var(--mv-chart-light-color, #000000);
+        --dark-color: var(--mv-chart-dark-color, #ffffff);
+      }
+
+      .mv-chart {
+        margin: var(--chart-margin);
+        height: var(--chart-height);
+        width: var(--chart-width);
+        position: relative;
+      }
+
+      .light {
+        background-color: var(--light-background);
+        color: var(--light-color);
+      }
+
+      .dark {
+        background-color: var(--dark-background);
+        color: var(--dark-color);
+      }
+
+      /* Chart.js custom styles 
+       *
+       * DOM element rendering detection
+       * https://davidwalsh.name/detect-node-insertion
+       */
+      @keyframes chartjs-render-animation {
+        from {
+          opacity: 0.99;
+        }
+        to {
+          opacity: 1;
+        }
+      }
+
+      .chartjs-render-monitor {
+        animation: chartjs-render-animation 0.001s;
+      }
+
+      /*
+       * DOM element resizing detection
+       * https://github.com/marcj/css-element-queries
+       */
+      .chartjs-size-monitor,
+      .chartjs-size-monitor-expand,
+      .chartjs-size-monitor-shrink {
+        position: absolute;
+        direction: ltr;
+        left: 0;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        overflow: hidden;
+        pointer-events: none;
+        visibility: hidden;
+        z-index: -1;
+      }
+
+      .chartjs-size-monitor-expand > div {
+        position: absolute;
+        width: 1000000px;
+        height: 1000000px;
+        left: 0;
+        top: 0;
+      }
+
+      .chartjs-size-monitor-shrink > div {
+        position: absolute;
+        width: 200%;
+        height: 200%;
+        left: 0;
+        top: 0;
+      }
+
+
+      .doughnut.large {
+        --mv-container-padding: 0 20px;
+        --mv-container-min-width: 400px;
+        --mv-container-max-width: 400px;
+        --mv-container-min-height: 570px;
+      }
+
+      .doughnut.large .gradient-test {
+        --mv-chart-height: 400px;
+        --mv-chart-width: 400px;
+        height: 400px;
+        width: 400px;
+      }
+
+      .doughnut.small {
+        --mv-container-padding: 0 20px;
+        --mv-container-min-width: 200px;
+        --mv-container-max-width: 200px;
+        --mv-container-min-height: 280px;
+        --mv-container-max-height: 280px;
+      }
+
+      .doughnut.small .gradient-test {
+        --mv-chart-height: 200px;
+        --mv-chart-width: 200px;
+        height: 200px;
+        width: 200px;
+      }
+
+    /*donut chart*/
+
+
+
+    .circle-bubble-1 {
+        width: 380px;
+        height: 380px;
+        margin: auto;
+        background-color: #c0e0e5;
+        border-radius: 50%;
+        position: relative !important;
+        margin: auto;
+        top: 100px;
+      }
+      .circle-bubble-2 {
+        width: 350px;
+        height: 350px;
+        margin: auto;
+        background-color: #dce9f2;
+        border-radius: 50%;
+        position: relative;
+        top: 15px;
+      }
+      .circle-bubble-3 {
+        width: 300px;
+        height: 300px;
+        margin: auto;
+        background-color: #fff;
+        box-shadow: 0px 15px 15px #ccc;
+        border-radius: 50%;
+        position: relative;
+        top: 25px;
+      }
+
+
+
+
+
+
+      .center {
+        width: 450px;
+        height: 450px;
+        background-color: #dce9f2;
+        border-radius: 50%;
+        position: absolute;
+        left: 50%;
+        margin-left: -225px;
+        top: 25px;
+        z-index: 999;
+      }
+      .inner {
+        width: 400px;
+        height: 400px;
+        background-color: rgb(69, 141, 183);
+        border-radius: 50%;
+        position: absolute;
+        left: 50%;
+        margin-left: -199px;
+        top: 25px;
+        z-index: 9991;
+        color: #fff;
+        text-align: center;
+
+        font-weight: bold;
+        box-shadow: 0px 10px 10px #ccc;
+      }
+
+      .back {
+        width: 600px;
+        height: 600px;
+        background-color: #dce9f2;
+        border-radius: 50%;
+        position: absolute;
+        left: 50%;
+        margin-left: -300px;
+        top: 45px;
+        z-index: 0;
+        box-shadow: 0px 10px 10px #ccc;
+      }
+
+      .back-stroke {
+        width: 700px;
+        height: 700px;
+        border-radius: 50%;
+        position: absolute;
+        left: 50%;
+        margin-left: -350px;
+        top: -100px;
+        z-index: 0;
+       /* box-shadow: rgb(204 204 204) 0px 10px 10px; */
+        border: 5px solid rgb(204, 204, 204);
+      }
+
+      .inner span:first-child {
+        font-size: 30px;
+
+        position: relative;
+        top: 70px;
+      }
+
+      .inner .result {
+        font-size: 150px;
+        top: 50px;
+        position: relative;
+    }
+
+
+      .inner .title {
+        font-size: 40px;
+        position: relative;
+        top: 50px;
+      }
+
+      .inner img {
+        width: 100px;
+        position: relative;
+        top: 40px;
+      }
+
+
+
+
+      .resultats {
+        border-radius: 50%;
+        position: absolute;
+        right: 0px;
+        font-size: 60px;
+        width: 150px;
+        height: 150px;
+        text-align: center;
+        top: 155px;
+        background-color: #fff;
+        color: #000;
+        text-decoration: none;
+        
+      }
+
+
+
+      .resultats > span > span {
+        font-size: 20px;
+      }
+
+
+      .result {
+        font-size: 100px;
+        width: 150px;
+        height: 150px;
+        text-align: center;
+      }
+
+
+      
+/*disposition des bulles*/
+.result-1-1{right:-160px !important;top:-145px;animation: fadein 1s; }
+
+.result-1-2{top: 135px !important;right: -275px !important ;animation: fadein 1s; }
+.result-2-2{top: 135px !important;left: -275px  !important; animation: fadein 4s;}
+
+.result-1-3{top: 165px !important; right: 0px  !important;animation: fadein 1s; }
+.result-2-3{left:145px !important;top: 500px !important;animation: fadein 3s;}
+.result-3-3{left:145px !important;top:-145px;animation: fadein 5s;}
+
+
+
+.result-1-4{right:-160px !important;top:-145px;animation: fadein 0s;}
+.result-2-4{right:-160px !important;top: 440px !important;animation: fadein 2s;}
+.result-3-4{left:-160px !important;top: 440px !important;animation: fadein 3s;}
+.result-4-4{left:-160px !important;top:-145px;animation: fadein 5s;}
+
+.result-1-5{right:-160px !important;top:-145px;animation: fadein 0s;}
+.result-2-5{top: 135px !important;right: -275px !important;animation: fadein 1s; }
+.result-3-5{right:-160px !important;top: 440px !important;animation: fadein 2s;}
+.result-4-5{left:-160px !important;top: 440px !important;animation: fadein 3s;}
+.result-5-5{top: 135px !important;left: -275px !important; animation: fadein 4s;}
+
+
+.result-1-6{right:-160px !important;top:-145px;animation: fadein 0s;}
+.result-2-6{top: 135px !important;right: -275px  !important;animation: fadein 1s; }
+.result-3-6{right:-160px !important;top: 440px !important;animation: fadein 2s;}
+.result-4-6{left:-160px !important;top: 440px !important;animation: fadein 3s;}
+.result-5-6{top: 135px !important;left: -275px  !important; animation: fadein 4s;}
+.result-6-6{left:-160px !important;top:-145px;animation: fadein 5s;}
+
+a.nolink:hover{cursor:context-menu !important;background-color:#fff;border-color:#DCE9F2 !important;}
+
+
+
+
+@keyframes fadein {
+  from {
+      opacity:0;
+  }
+  to {
+      opacity:1;
+  }
+}
+.resultats > span {
+position:relative;top:30px;
+}
+.resultats:hover{background-color:#DCE9F2;border-color:#fff !important;}
+
+
+.mv-chart-canvas {
+  width: 720px !important;
+  height: 535px !important;
+  display: block !important;
+  margin: auto;
+  left: -158px;
+  top: -14px;
+  position: relative;
+}
+
+
+
+    `;
+    }
+
+    constructor() {
+        super();
+        this.theme = "light";
+        this.chart = null;
+        this.valeur = null;
+    }
+
+    static get properties() {
+        return {
+            valeur: {
+                type: Array
+            }
+        };
+    }
+
+    render() {
+        return html `
+
+
+
+<div style="transform: scale(0.4);">
+<div class="back-stroke">
+              <div class="back"></div>
+            </div>
+            <div class="center">
+              <div class="inner">
+                <img src="./src/img/donut-img.svg" /><br /><span
+                  class="title"
+                  >Brands</span
+                ><br /><span class="result"
+                  >${DOUGHNUT_CONFIG.result}</span
+                >
+              </div>
+        
+
+${this.displayDonutBubbles()}
+
+</div>
+
+
+      <div class="mv-chart">
+        <canvas class="mv-chart-canvas"></canvas>
+      </div>
+      </div>
+
+
+
+
+    `;
+    }
+
+    firstUpdated() {
+
+
+     
+
+     
+        if (!this.chart) {
+       
+            const plugins = this.plugins || [];
+            plugins.push(ChartDataLabels);
+            const canvas = this
+                .shadowRoot
+                .querySelector(".mv-chart-canvas")
+                .getContext("2d");
+            this.chart = new Chart(canvas, DOUGHNUT_CONFIG);
+        }
+
+    }
+
+ 
+
+    displayDonutBubbles() {
+        let i;
+        let loop = new Array();
+        this.valeur = new Array();
+
+        let max = DOUGHNUT_CONFIG
+            .data
+            .datasets[0]
+            .data
+            .length;
+
+        for (i = 0; i < max; i++) {
+
+            if (DOUGHNUT_CONFIG.data.datasets[0].data[i]) {
+
+                this.valeur[i] = DOUGHNUT_CONFIG
+                    .data
+                    .datasets[0]
+                    .data[i];
+
+                if (DOUGHNUT_CONFIG.data.datasets[0].links[i] != '') {
+
+                    loop[i] = html `<a href="${DOUGHNUT_CONFIG
+                        .data
+                        .datasets[0]
+                        .links[i]}" target="_blank"  class="result-${i + 1}-${max} resultats pos"  style="border:solid 10px ${DOUGHNUT_CONFIG
+                        .data
+                        .datasets[0]
+                        .backgroundColor[i]};"><span><span>${DOUGHNUT_CONFIG
+                        .data
+                        .names[i]}</span><br /><strong id="value-${i}">${this
+                        .valeur[i]}</strong></span></a>`;
+                } else {
+
+                    loop[i] = html `<a class="result-${i + 1}-${max} resultats pos nolink"  style="border:solid 10px ${DOUGHNUT_CONFIG
+                        .data
+                        .datasets[0]
+                        .backgroundColor[i]};"><span><span>${DOUGHNUT_CONFIG
+                        .data
+                        .names[i]}</span><br /><strong id="value-${i}">${this
+                        .valeur[i]}</strong></span></a>`;
+
+                }
+            }
+
+        }
+        return loop;
+    }
+
+}
+
+customElements.define("mv-chart-donut", MvChart);
